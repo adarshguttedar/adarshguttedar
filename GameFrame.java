6fe775d3f9a07f06678b9c1a5f995c2ffda6ba1c
Adarsh <111170216+adarshguttedar@users.noreply.github.com>
2023-05-27 17:16:18 +0530

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+
+public class GameFrame extends JFrame{
+
+	GameFrame(){
+		GamePanel gamePanel = new GamePanel();
+		getContentPane().add(gamePanel, BorderLayout.SOUTH);
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+}
